238c60a981cb37636cac3f58ec9a6bb18ae88612
kibum <rlqja9611@naver.com>
2023-06-21 19:50:02 +0900

    MELT

--- shell/MELT_FMLP/test_amaozn.sh
+++ shell/MELT_FMLP/test_amaozn.sh
@@ -1,3 +1,3 @@
 gpu=2
 data="Music" # Clothing, Sports, Beauty, Grocery, Automotive, Music
-python main.py --inference true --model MELT_SASRec --batch_size 128 --dataset $data --gpu $gpu --pareto_rule 0.8
+python main.py --inference true --model MELT_FMLP --batch_size 128 --dataset $data --gpu $gpu --pareto_rule 0.8
--- shell/SASRec/test_amazon.sh
+++ shell/SASRec/test_amazon.sh
@@ -1,3 +1,3 @@
 gpu=2 # GPU num
 data="Music" # Clothing, Sports, Beauty, Grocery, Automotive, Music
-python main.py --inference true --model FMLP --batch_size 128 --dataset $data --gpu $gpu --pareto_rule 0.8
+python main.py --inference true --model SASRec --batch_size 128 --dataset $data --gpu $gpu --pareto_rule 0.8
